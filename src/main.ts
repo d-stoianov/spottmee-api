c3066c91932e53a287d5b71c8f6fa65bcd4cfda3
Dmytro Stoianov <dmytrostoianov05@gmail.com>
2025-08-29 23:24:25 +0200

    Add global zod validation filter for catching zod errors

--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,17 @@ import { NestFactory } from '@nestjs/core'
 import { ValidationPipe } from '@nestjs/common'
 
 import { AppModule } from '@/app.module'
+import { ZodValidationExceptionFilter } from '@/zod-validation.filter'
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule)
     app.useGlobalPipes(
         new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }),
     )
+
     app.enableCors(process.env.origin ?? '*')
+    app.useGlobalFilters(new ZodValidationExceptionFilter())
+
     await app.listen(process.env.PORT ?? 3000)
 }
 
---
+++ src/zod-validation.filter.ts
@@ -0,0 +1,23 @@
+import {
+    ExceptionFilter,
+    Catch,
+    ArgumentsHost,
+} from '@nestjs/common'
+import { Response } from 'express'
+import { ZodError } from 'zod'
+
+@Catch(ZodError)
+export class ZodValidationExceptionFilter implements ExceptionFilter {
+    catch(exception: ZodError, host: ArgumentsHost) {
+        const ctx = host.switchToHttp()
+        const response = ctx.getResponse<Response>()
+
+        const messages = exception.issues.map((issue) => issue.message)
+
+        response.status(400).json({
+            statusCode: 400,
+            message: messages,
+            error: 'Bad Request',
+        })
+    }
+}
